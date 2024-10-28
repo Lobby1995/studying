a6c21c1197b2a83b5212fbb801db875dbd7a8e32
Lobby1995 <isakovalex.1995@gmail.com>
2024-10-28 16:46:05 +0200

    Delete personalstudy.py

--- personalstudy.py
+++
@@ -1,4 +0,0 @@
-print("welcome to the band name generator")
-city = input("what is the name of the city your grew up in?\n")
-pet_name = input("what is your pet's name?\n")
-print("your band name could be: " + city + " " + pet_name)
