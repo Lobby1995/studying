73debde74b0b98a5d5fd4c2246551ff2c31d0d23
Lobby1995 <isakovalex.1995@gmail.com>
2024-10-28 16:44:15 +0200

    Add files via upload

---
+++ personalstudy.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city = input("what is the name of the city your grew up in?\n")
+pet_name = input("what is your pet's name?\n")
+print("your band name could be: " + city + " " + pet_name)
